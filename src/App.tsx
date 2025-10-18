2e9bfd700fb32e4ac08dd05917a810056452db40
monsterchick <kwanho.app@gmail.com>
2025-10-18 14:03:10 +0800

    chore: installed shadcn components library

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 className="border-2 border-red-500 p-4 bg-blue-100">Hello world!</h1>
+      <h1 className="border-2 border-blue-500 p-4 bg-blue-100">Hello world!</h1>
     </>
   )
 }
