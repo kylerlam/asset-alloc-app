750d283330fd8d81d0ff52fee6bac6eee36b92c9
monsterchick <kwanho.app@gmail.com>
2025-10-18 16:06:02 +0800

    chore: init TanStack Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button'
 function App() {
 
   return (
     <>
       <h1 className="border-2 border-blue-500 p-4 bg-blue-100">Hello world!</h1>
+      <Button variant="outline">Outline</Button>
     </>
   )
 }
